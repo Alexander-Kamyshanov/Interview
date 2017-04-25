dcc33cf9e7c4d58421a0d6f07b29e544521a627d
kamyshanov <kamyshanov@outlook.com>
2017-04-25 22:12:47 +0300

    task 1 solution

---
+++ MultiThreadQueue/Collections/ThreadSafetyQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace MultiThreadQueue.Collections
+{
+    public class ThreadSafetyQueue<T>
+    {
+        private readonly Queue<T> _internalQueue = new Queue<T>();
+
+        public void Push(T item)
+        {
+            lock (_internalQueue)
+            {
+                _internalQueue.Enqueue(item);
+                Monitor.Pulse(_internalQueue);
+            }
+
+        }
+
+        public T Pop()
+        {
+            lock (_internalQueue)
+            {
+                while (!_internalQueue.Any())
+                {
+                    Monitor.Wait(_internalQueue);
+                }
+                T result = _internalQueue.Dequeue();
+                Monitor.Pulse(_internalQueue);
+                return result;
+            }
+        }
+
+        public override string ToString()
+        {
+            lock (_internalQueue)
+            {
+                return String.Join(",", _internalQueue);
+            }
+        }
+    }
+}
--- MultiThreadQueue/Program.cs
+++ MultiThreadQueue/Program.cs
@@ -2,14 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using MultiThreadQueue.Collections;
 
 namespace MultiThreadQueue
 {
     class Program
     {
+        static int interationsCount = 3;
+        static readonly ThreadSafetyQueue<int> queue = new ThreadSafetyQueue<int>();
         static void Main(string[] args)
         {
+
+            for (int i = 0; i < interationsCount; i++)
+            {
+                Thread thread;
+                if (i % 2 == 1)
+                {
+                    thread = new Thread(new ParameterizedThreadStart(Populate));
+                    thread.Start(i);
+                }
+                else
+                {
+                    thread = new Thread(new ParameterizedThreadStart(Remove));
+                    thread.Start(i);
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+
+        public static void Populate(object index)
+        {
+            int ind = (int)index;
+            string name = $"Thread {ind}";
+            for (int i = ind; i < interationsCount + ind; i++)
+            {
+                Console.WriteLine($"{name} is trying to push {i}");
+                queue.Push(i);
+                Console.WriteLine($"Populate result: {queue}");
+                Thread.Sleep(i * 100);
+            }
+        }
+
+        public static void Remove(object index)
+        {
+            string name = $"Thread {(int) index}";
+            Console.WriteLine($"{name} is trying to pop");
+            var result = queue.Pop();
+            Console.WriteLine($"{name} result: {result}");
+            Thread.Sleep(100 + (int)index * 100);
         }
     }
 }
