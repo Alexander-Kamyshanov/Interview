7bf20ca543d21050beb2bca24421f12d23091fe4
kamyshanov <kamyshanov@outlook.com>
2017-04-25 22:15:48 +0300

    Task 2 initial import

---
+++ NumbericPairSearch/Model/Pair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbericPairSearch.Model
+{
+    public struct Pair
+    {
+        public int Left;
+        public int Right;
+    }
+}
