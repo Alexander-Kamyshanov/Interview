ce6162ee5c7cc5747f99bdc5f9dc765d8309dca8
kamyshanov <kamyshanov@outlook.com>
2017-04-25 20:29:54 +0300

    Initial import

---
+++ MultiThreadQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
