0135bbfbf7e16ae7e3371b0a7f3812b7897c2871
kamyshanov <kamyshanov@outlook.com>
2017-04-25 23:04:31 +0300

    SearchHelper implementation

--- NumbericPairSearch/Helpers/SearchHelper.cs
+++ NumbericPairSearch/Helpers/SearchHelper.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NumbericPairSearch.Model;
 
 namespace NumbericPairSearch.Helpers
 {
-    class SearchHelper
+    public static class SearchHelper
     {
+        public static List<Pair> FindPairs(List<int> source, int sum)
+        {
+            if (source == null)
+            {
+                throw new ArgumentException(nameof(source));
+            }
+
+            List<Pair> result = new List<Pair>();
+
+            List<int> cleanedSource = SortAndClean(source);
+
+            // nothing to check
+            if (cleanedSource.Count < 2)
+            {
+                return result;
+            }
+
+            int leftIterator = 0;
+            int rightIterator = cleanedSource.Count - 1;
+
+            // if max elem + it's previous element < sum - nothing to search
+            if ((cleanedSource[rightIterator] + cleanedSource[rightIterator - 1]) < sum)
+            {
+                return result;
+            }
+
+            while (leftIterator < rightIterator)
+            {
+                // current min
+                int currentLeft = cleanedSource[leftIterator];
+                // current max 
+                int currentRight = cleanedSource[rightIterator];
+
+                // skip each current max element if current max + current min > sum
+                while ((currentRight + currentLeft) > sum && rightIterator > 0)
+                {
+                    currentRight = cleanedSource[--rightIterator];
+                }
+
+                if (currentLeft + currentRight == sum)
+                {
+                    result.Add(new Pair { Left = currentLeft, Right = currentRight });
+                }
+
+                leftIterator++;
+            }
+
+            return result;
+        }
+
+        private static List<int> SortAndClean(List<int> source)
+        {
+            List<int> result = new List<int>();
+            int? last = null;
+            foreach (int item in source.OrderBy(si => si))
+            {
+                if (last != item)
+                {
+                    result.Add(item);
+                }
+                last = item;
+            }
+            return result;
+        }
     }
 }
--- NumbericPairSearch/Program.cs
+++ NumbericPairSearch/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NumbericPairSearch.Helpers;
+using NumbericPairSearch.Model;
 
 namespace NumbericPairSearch
 {
@@ -10,6 +9,26 @@ namespace NumbericPairSearch
     {
         static void Main(string[] args)
         {
+            const int collectionLength = 100;
+            const int collectionBound = 100;
+            const int sum = 70;
+            Console.WriteLine($"Collection length = {collectionLength}");
+            Console.WriteLine($"Collection bound = {collectionBound}");
+            Console.WriteLine($"Sum = {collectionLength}");
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            List<int> source = new List<int>();
+            for (int i = 0; i < collectionLength; i++)
+            {
+                source.Add(rnd.Next(-collectionBound, collectionBound));
+            }
+            Console.WriteLine($"Collection: [{String.Join(", ", source)}]");
+
+            foreach (Pair pair in SearchHelper.FindPairs(source, sum))
+            {
+                Console.WriteLine($"{pair.Left} + {pair.Right} = {sum}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
